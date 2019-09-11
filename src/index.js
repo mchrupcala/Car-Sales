4128cc4d76a687ebf6fbc78ccc30472efb0f922b
mchrupcala <michaelchrupcala@Michaels-MacBook-Pro.local>
2019-09-11 16:21:01 -0400

    Downloaded all my dependencies. Set up initialstate and a reducer. Connected my store & reducer in my index.js plus a Provider to route everything.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { reducer } from './reducers/reducer';
 import App from './App';
 
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+
+const store = createStore(reducer);
+
+
+ReactDOM.render(
+<Provider store={store}>
+    <App />
+</Provider>, rootElement);
